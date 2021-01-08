56da85a7242b8d2e79f3dd4e094c02c0f9d90812
tribbleofjim <lyifee@qq.com>
2021-01-08 14:11:51 +0800

    add dao

---
+++ maple-worker/src/main/java/com/webmaple/worker/dao/SpiderDAO.java
@@ -0,0 +1,16 @@
+package com.webmaple.worker.dao;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.annotation.Resource;
+
+/**
+ * @author lyifee
+ * on 2021/1/8
+ */
+public class SpiderDAO {
+    @Resource
+    private JdbcTemplate jdbcTemplate;
+
+
+}
