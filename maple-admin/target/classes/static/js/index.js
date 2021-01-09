61086481c263bc87dcdf74ee577d2caa0a66a2ca
tribbleofjim <lyifee@qq.com>
2021-01-09 16:59:47 +0800

    edit readme

--- maple-admin/target/classes/static/js/index.js
+++
@@ -1,7 +0,0 @@
-layui.use(['form', 'layer','table', 'jquery', 'element'], function () {
-    let form = layui.form;
-    let layer = layui.layer;
-    let table = layui.table;
-    let element = layui.element;
-    let $ = layui.jquery;
-}
