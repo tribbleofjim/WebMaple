3a85f88d4df09b13bfc5b2ebf47aded3b083b40c
tribbleofjim <lyifee@qq.com>
2021-01-09 16:42:51 +0800

    add network module

---
+++ maple-worker/src/main/java/com/webmaple/worker/network/MethodController.java
@@ -0,0 +1,11 @@
+package com.webmaple.worker.network;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author lyifee
+ * on 2021/1/9
+ */
+@Controller
+public class MethodController {
+}
