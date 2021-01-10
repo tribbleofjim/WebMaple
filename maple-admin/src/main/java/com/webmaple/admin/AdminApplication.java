5230d980db1787ac369ccb6072b6246ed33942d5
tribbleofjim <lyifee@qq.com>
2021-01-10 17:00:48 +0800

    adjust style of index

--- maple-admin/src/main/java/com/webmaple/admin/AdminApplication.java
+++ maple-admin/src/main/java/com/webmaple/admin/AdminApplication.java
@@ -2,12 +2,13 @@ package com.webmaple.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * @author lyifee
  * on 2021/1/6
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class AdminApplication {
     public static void main(String[] args) {
         SpringApplication.run(AdminApplication.class);
