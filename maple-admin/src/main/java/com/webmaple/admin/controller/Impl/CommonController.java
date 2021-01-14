65f150c6a524b793665598a8afaf67fb961257cf
tribbleofjim <lyifee@qq.com>
2021-01-14 20:27:39 +0800

    add pages

--- maple-admin/src/main/java/com/webmaple/admin/controller/Impl/CommonController.java
+++ maple-admin/src/main/java/com/webmaple/admin/controller/Impl/CommonController.java
@@ -13,7 +13,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import javax.annotation.Resource;
 import java.util.List;
 
