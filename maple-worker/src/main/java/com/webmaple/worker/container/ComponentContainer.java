a1e456efa787e7b25276e8a62bb13dea840b5956
tribbleofjim <lyifee@qq.com>
2021-01-20 11:43:38 +0800

    component methods

--- maple-worker/src/main/java/com/webmaple/worker/container/ComponentContainer.java
+++ maple-worker/src/main/java/com/webmaple/worker/container/ComponentContainer.java
@@ -1,19 +1,58 @@
 package com.webmaple.worker.container;
 
-import java.util.ArrayList;
+import com.webmaple.worker.util.RedisUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import java.util.List;
 
 /**
  * @author lyifee
  * on 2021/1/20
  */
+@Component
 public class ComponentContainer {
-    List<String> downloaders = new ArrayList<>();
 
-    List<String> processors = new ArrayList<>();
+    @Autowired
+    private RedisUtil redisUtil;
 
-    List<String> pipelines = new ArrayList<>();
+    public void addProcessors(List<String> processorList) {
 
+    }
 
+    public void addDownloaders(List<String> downloaderList) {
 
+    }
+
+    public void addPipelines(List<String> pipelineList) {
+
+    }
+
+    public List<String> getProcessors() {
+        return null;
+    }
+
+    public List<String> getDownloaders() {
+        return null;
+    }
+
+    public List<String> getPipelines() {
+        return null;
+    }
+
+    public void removeProcessor(String processor) {
+
+    }
+
+    public void removeDownloader(String downloader) {
+
+    }
+
+    public void removePipeline(String pipeline) {
+
+    }
+
+    public void destroy() {
+
+    }
 }
