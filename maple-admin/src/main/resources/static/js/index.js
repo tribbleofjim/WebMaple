844804e923648d9765a46922487d4cc45d487977
tribbleofjim <lyifee@qq.com>
2021-01-31 13:06:25 +0800

    add week visits

--- maple-admin/src/main/resources/static/js/index.js
+++ maple-admin/src/main/resources/static/js/index.js
@@ -1,7 +1,31 @@
-layui.use(['form', 'layer','table', 'jquery', 'element'], function () {
+layui.use(['form', 'layer','table', 'jquery', 'element','carousel'], function () {
     let form = layui.form;
     let layer = layui.layer;
     let table = layui.table;
     let element = layui.element;
+    let carousel = layui.carousel;
     let $ = layui.jquery;
+
+    carousel.render({
+        elem: '#carousel'
+        ,width: '100%' //设置容器宽度
+        ,arrow: 'always' //始终显示箭头
+    });
 });
+
+var myChart = echarts.init(document.getElementById('visits', 'dark'));
+var option = {
+    color: ['#008B8B'],
+    xAxis: {
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    },
+    yAxis: {
+        type: 'value'
+    },
+    series: [{
+        data: [150, 230, 224, 218, 135, 147, 260],
+        type: 'line'
+    }]
+};
+option && myChart.setOption(option);
