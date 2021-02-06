e26b863784e5777d4ab6b34be605c33673b264d3
tribbleofjim <lyifee@qq.com>
2021-02-06 21:21:51 +0800

    add user auth

--- maple-admin/src/main/resources/static/js/auth.js
+++ maple-admin/src/main/resources/static/js/auth.js
@@ -1,11 +1,31 @@
 document.getElementById("username").innerHTML = "用户：" + decodeURIComponent(getCookieByKey("user"));
 
-var layer,form, element, $;
-layui.use(['layer', 'form', 'jquery', 'element'], function () {
+var gotAuths = [
+    {"value": "1", "title": "server1", "disabled": "", "checked": ""}
+    ,{"value": "2", "title": "mysql1", "disabled": "", "checked": ""}
+    ,{"value": "3", "title": "server2", "disabled": "", "checked": ""}
+    ,{"value": "4", "title": "mongo1", "disabled": "", "checked": ""}
+    ,{"value": "5", "title": "redis1", "disabled": "", "checked": ""}
+    ,{"value": "6", "title": "redis2", "disabled": "", "checked": ""}
+]
+
+var value = ["4", "5", "6"]
+
+var layer,form, element, transfer, $;
+layui.use(['layer', 'form', 'jquery', 'element', 'transfer'], function () {
     layer = layui.layer;
     element = layui.element;
+    transfer = layui.transfer;
     form = layui.form;
     $ = layui.jquery;
+
+    transfer.render({
+        elem: '#sourceAuths'  //绑定元素
+        ,title: ['未获取权限', '已获取权限']
+        ,data: gotAuths
+        ,value: value
+        ,id: 'sourceAuths' //定义索引
+    });
 });
 
 function addSource() {
@@ -31,6 +51,33 @@ function addSource() {
                     sourceIp: sourceIp,
                     account: account,
                     pass: pass
+                },
+                success: function (res) {
+                    layer.msg(res.message);
+                }
+            });
+            return true;
+        },
+        cancel: function(layero,index){
+            layer.closeAll();
+        }
+    });
+}
+
+function editAuth() {
+    layer.open({
+        type:1,
+        area:['570px','530px'],
+        title: '用户授权',
+        content: $("#editAuth"),
+        shade: 0,
+        btn: '提交'
+        ,yes: function(index, layero) {
+            $.ajax({
+                type: 'get',
+                url: "editAuth",
+                data: {
+
                 },
                 success: function (res) {
                     layer.msg(res.message);
