4f46b23682431f0b5d00a3db1a4e3c928802e44b
tribbleofjim <lyifee@qq.com>
2021-02-09 15:58:23 +0800

    create template

--- maple-admin/src/main/java/com/webmaple/admin/mapper/TemplateMapper.java
+++ maple-admin/src/main/java/com/webmaple/admin/mapper/TemplateMapper.java
@@ -2,7 +2,9 @@ package com.webmaple.admin.mapper;
 
 import com.webmaple.admin.model.Template;
 import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
@@ -10,6 +12,8 @@ import java.util.List;
  * @author lyifee
  * on 2021/2/9
  */
+@Mapper
+@Component
 public interface TemplateMapper {
     @Insert("INSERT INTO maple_template (html, template_name, form_url) " +
             "VALUES (#{html}, #{templateName}, #{formUrl})")
