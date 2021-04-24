42cb7dcdbb643d25e6d69a0bfa86c4fe3d31be9d
tribbleofjim <lyifee@qq.com>
2021-04-24 14:12:37 +0800

    add database

--- maple-admin/src/main/resources/table.sql
+++ maple-admin/src/main/resources/table.sql
@@ -26,24 +26,6 @@ CREATE TABLE `maple_template` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- ----------------------------
--- Table structure for maple_user
--- ----------------------------
-DROP TABLE IF EXISTS `maple_user`;
-CREATE TABLE `maple_user` (
-  `phone` varchar(255) NOT NULL,
-  `nickname` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `auth` enum('0','1') DEFAULT '1' COMMENT '0-管理员账号，1-普通用户账号',
-  PRIMARY KEY (`phone`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
--- ----------------------------
--- Records of maple_user
--- ----------------------------
-BEGIN;
-INSERT INTO `maple_user` VALUES ('123456', 'admin', 'admin', '0');
-COMMIT;
-
--- ----------------------------
 -- Table structure for maple_user_source
 -- ----------------------------
 DROP TABLE IF EXISTS `maple_user_source`;
@@ -54,4 +36,51 @@ CREATE TABLE `maple_user_source` (
   PRIMARY KEY (`phone`,`source_name`,`source_type`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- ----------------------------
+-- Table structure for maple_spider
+-- ----------------------------
+DROP TABLE IF EXISTS `maple_spider`;
+CREATE TABLE `maple_spider` (
+  `worker` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `urls` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `uuid` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `state` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `thread_num` int(11) DEFAULT NULL,
+  `processor` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `downloader` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `pipeline` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`worker`,`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Table structure for maple_message
+-- ----------------------------
+DROP TABLE IF EXISTS `maple_message`;
+CREATE TABLE `maple_message` (
+  `from_user` varchar(255) NOT NULL,
+  `to_user` varchar(255) NOT NULL,
+  `content` varchar(255) DEFAULT NULL,
+  `valid` binary(0) DEFAULT NULL,
+  PRIMARY KEY (`from_user`,`to_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Table structure for maple_user
+-- ----------------------------
+DROP TABLE IF EXISTS `maple_user`;
+CREATE TABLE `maple_user` (
+  `phone` varchar(255) NOT NULL,
+  `nickname` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `auth` enum('0','1') DEFAULT '1' COMMENT '0-管理员账号，1-普通用户账号',
+  PRIMARY KEY (`phone`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 SET FOREIGN_KEY_CHECKS = 1;
+
+-- ----------------------------
+-- Records of maple_user
+-- ----------------------------
+BEGIN;
+INSERT INTO `maple_user` VALUES ('123456', 'admin', 'admin', '0');
+COMMIT;
